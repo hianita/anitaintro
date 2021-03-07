cb17861bd9823b07a44aebb1e0b72673fbfa5838
hianita <anitalin0309@gmail.com>
2021-03-07 17:30:01 +0800

    Add files via upload

--- lesson2/pt3.py
+++ lesson2/pt3.py
@@ -1,14 +1,11 @@
 def maxProduct(nums):
-    max = 0
     op = 0
+    set=[]
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
             op=nums[i]*nums[j]
-            if op > max:
-                max = op
-    print(max)
+            set.append(op)
+    print(max(set))
 
 maxProduct([5,20,2,6])
 maxProduct([10,-20,0,3])
-
-
